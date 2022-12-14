dc81cf649ee3dca8baead56cdb654c36f8603eb1
横山 <96154391+YokoyamaYuta@users.noreply.github.com>
2022-12-15 03:16:35 +0900

    光の処理、アイテム

---
+++ Hakkason_season8/Assets/Member/Yuta/Scripts/Test_GameManger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_GameManger : MonoBehaviour
+{
+    [SerializeField] GameObject lightObject;
+    private float SpotAngle;  //ライトの大きさ
+    public static bool Item = false;  //アイテム取得フラグ
+    private float time = 0f;  //時間経過記録
+    private bool GameOver = false;  //ゲームオーバーフラグ
+
+    void Start()
+    {
+        //スポットライトのコンポーネント取得
+        SpotAngle = lightObject.GetComponent<Light>().spotAngle;
+        SpotAngle = 30.0f;  //初期の大きさ
+        time = 0f;
+        Item = false;
+        GameOver = false;
+    }
+
+    void Update()
+    {
+        //ライトの大きさ反映
+        lightObject.GetComponent<Light>().spotAngle = SpotAngle;
+
+        time += Time.deltaTime;
+
+        //3秒経つか、ゲームオーバーでは無かったら
+        if (time >= 3f && !GameOver)
+        {
+            //ライトを小さくする
+            SpotAngle -= 1f;
+            time = 0f;
+        }
+
+        //アイテムを取ったら
+        if (Item)
+        {
+            //ライトの大きさを初期に戻す
+            SpotAngle = 30.0f;
+            Item = false;
+        }
+
+        //ライトの大きさが5以下だったら
+        if (SpotAngle <= 5f)
+        {
+            //終わりにする
+            GameOver = true;
+            Debug.Log("終わり");
+        }
+    }
+}
---
+++ Hakkason_season8/Assets/Member/Yuta/Scripts/Test_Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ÉvÉåÉCÉÑÅ[ÇÃâºëÄçÏ
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0.0f, 0.0f, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0.0f, 0.0f, -moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            Test_GameManger.Item = true;
+            Destroy(collision.gameObject);
+        }
+    }
+}
