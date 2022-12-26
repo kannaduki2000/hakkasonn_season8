60a800efd02802ba9ce171da11b994360f8bcd25
kikuchitoya <21c1020024kt@edu.tech.ac.jp>
2022-12-27 01:49:28 +0900

    Playerの移動の仕様の変更、待機、移動アニメーションの作成がとりあえず完成しました。確認お願いします。

--- Hakkason_season8/Assets/Kikuchi/Scripts/PlayerMove.cs
+++ Hakkason_season8/Assets/Kikuchi/Scripts/PlayerMove.cs
@@ -5,55 +5,54 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1.0f;
+    private float speed = 3.0f;
+    private float moveX = 0.0f;
+	private float moveZ = 0.0f;
+    // 向き関係
+    private Vector3 ySpeed;
+    private Vector3 latestPos;
+    private Vector3 nowPos;
+
+    //アニメーター
+    Animator animator;
+
+	Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody> ();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W)) 
 
-        { 
+        moveX = Input.GetAxis ("Horizontal") * speed;
+        moveZ = Input.GetAxis ("Vertical") * speed;
 
-            this.transform.Translate(0.0f,0.0f,speed * Time.deltaTime); 
+        if( moveX > 0.1 || moveZ > 0.1 || moveX < -0.1 || moveZ < -0.1)
+        {
+            animator.SetBool("Iswalking",true);
+        }
+        else
+        {
+            animator.SetBool("Iswalking",false);
+        }
 
-        } 
+        nowPos = this.transform.position - latestPos;
+        nowPos.y = 0;
+        latestPos = this.transform.position;
+        if (nowPos.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(nowPos); //向きを変更する
+        }
+        
+    }
 
- 
- 
-
-        if(Input.GetKey(KeyCode.S)) 
-
-        { 
-
-            this.transform.Translate(0.0f,0.0f,-speed * Time.deltaTime); 
-
-        } 
-
- 
- 
-
-        if(Input.GetKey(KeyCode.A)) 
-
-        { 
-
-            this.transform.Translate(-speed * Time.deltaTime,0.0f,0.0f); 
-
-        } 
-
- 
- 
-
-        if(Input.GetKey(KeyCode.D)) 
-
-        { 
-
-            this.transform.Translate(speed * Time.deltaTime,0.0f,0.0f); 
-
-        } 
+    void FixedUpdate()
+    {
+        ySpeed = rb.velocity;
+        rb.velocity = new Vector3(moveX,ySpeed.y, moveZ);
     }
 }
